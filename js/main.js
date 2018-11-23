c706f646d5af0de0d68265ca778882a07e164f20
Yến Đặng <dangthiyen103@gmail.com>
2018-11-23 11:46:45 +0700

    Completed Product Page
    Completed Product Page, Change several css style in index file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
+// ----------------------------------------------
+// Fixed Header When Scrolling
+window.onscroll = function() {myFunction()};
 
+var header = document.getElementById("header");
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+}
+// --------------------------------------------------
+
+
+// -----------------------------------------------
 // Custome slick carousel
     $('.slick-carousel1').slick({
         dots: false,
@@ -119,103 +137,13 @@
 
 
 // -----------------------------------------------
-// Fullscreen header
-jQuery(document).ready(function($){
- // Defining a function to set size for #hero 
-    function fullscreen(){
-        jQuery('#hero-header').css({
-            width: jQuery(window).width(),
-            height: jQuery(window).height()
-        });
-    }
-  
-    fullscreen();
-
-  // Run the function in case of window resize
-  jQuery(window).resize(function() {
-       fullscreen();         
-    });
-
-});
-
 
 
 
 // --------------------------------------------------------------
-// Binding to the scroll event of the container (window)
-// Cache selectors
-var lastId,
-    topMenu = $("#ait-header"),
-    topMenuHeight = topMenu.outerHeight()+15,
-    // All list items
-    menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function(){
-      var item = $($(this).attr("href"));
-      if (item.length) { return item; }
-    });
+// Left Side Bar
 
-// Bind to scroll
-$(window).scroll(function(){
-   // Get container scroll position
-   var fromTop = $(this).scrollTop()+topMenuHeight;
-   
-   // Get id of current scroll item
-   var cur = scrollItems.map(function(){
-     if ($(this).offset().top < fromTop)
-       return this;
-   });
-   // Get the id of the current element
-   cur = cur[cur.length-1];
-   var id = cur && cur.length ? cur[0].id : "";
-   
-   if (lastId !== id) {
-       lastId = id;
-       // Set/remove active class
-       menuItems
-         .parent().removeClass("active")
-         .end().filter("[href='#"+id+"']").parent().addClass("active");
-   }                   
-});
-
-
-
-
-// ------------------------------------------------------------------ 
-// Scroll to the selected anchor
-$(document).ready(function(){
-	// Add smooth scrolling to all links
-	$("a").on('click', function(event) {
-
-	   	//removing the previous selected menu state
-        $('.navbar-nav').find('li.active').removeClass('active');
-        //adding the state for this parent menu
-        $(this).parents("li").addClass('active');
-
-	    // Make sure this.hash has a value before overriding default behavior
-	    if (this.hash !== "") {
-	      	// Prevent default anchor click behavior
-	      	event.preventDefault();
-
-	      	// Store hash
-	     	var hash = this.hash;
-
-	      	// Using jQuery's animate() method to add smooth page scroll
-	      	// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-	      	if($(this).attr('href') == '#cach-su-dung' || $(this).attr('href') == '#lien-he'){
-	      		$('html, body').animate({
-		       	 	scrollTop: $(hash).offset().top
-		      	}, 1000);
-	      	}
-	      	else{
-	      		$('html, body').animate({
-		        	scrollTop: $(hash).offset().top-50
-		      	}, 1000);
-	      	}
-	      
-	    } // End if
-	});
-});
+// -----------------------------------------------------
 
 
 
