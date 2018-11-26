b7af102b065fd9c4957f2ce8ce034aa466b3bd9f
Yến Đặng <dangthiyen103@gmail.com>
2018-11-26 16:08:41 +0700

    Completed Product Detail Page

--- js/main.js
+++ js/main.js
@@ -136,12 +136,43 @@ function myFunction() {
     });
 
 
+    $('.slick3').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        dots: true,
+        appendDots: $('.wrap-slick3-dots'),
+        dotsClass:'slick3-dots',
+        infinite: true,
+        autoplay: false,
+        autoplaySpeed: 6000,
+        arrows: false,
+        customPaging: function(slick, index) {
+            var portrait = $(slick.$slides[index]).data('thumb');
+            return '<img src=" ' + portrait + ' "/><div class="slick3-dot-overlay"></div>';
+        },  
+    });
+
+
 // -----------------------------------------------
 
 
 
 // --------------------------------------------------------------
-// Left Side Bar
+// *[ +/- num product ]
+// --------------------------------------------------------------
+$('.btn-num-product-down').on('click', function(e){
+    e.preventDefault();
+    var numProduct = Number($(this).next().val());
+    if(numProduct > 1) $(this).next().val(numProduct - 1);
+});
+
+$('.btn-num-product-up').on('click', function(e){
+    e.preventDefault();
+    var numProduct = Number($(this).prev().val());
+    $(this).prev().val(numProduct + 1);
+});
+
 
 // -----------------------------------------------------
 
